7adc580302dd87fa58ad7deb436862a591ea90c5
Sammie156 <samanwayadatta@gmail.com>
2025-06-03 14:36:14 +0530

    Basic cosmetic change

--- src/components/Product.jsx
+++ src/components/Product.jsx
@@ -1,20 +1,20 @@
-import '../styles/Product.css';
+import "../styles/Product.css";
 
 function Product(props) {
-  
   return (
     <div className="product-card">
       <h3 className="product-name">{props.name}</h3>
       <div className="image-container">
-        {props.image.map(image => (
-          <img src={image} className='product-image'/>
+        {props.image.map((image) => (
+          <img src={image} className="product-image" />
         ))}
       </div>
       <p className="product-desc">{props.description}</p>
 
       <p className="product-price">${props.price}</p>
-      <div>
+      <div className="buttons">
         <p className="product-cat">Category: {props.category}</p>
+        <button type="button">Remove Item</button>
       </div>
     </div>
   );
